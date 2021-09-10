b5a915b312328ccce77b94b0206b5ccc11538631
Léto Gaudet dit Trafit <lgaudet-@student.42lyon.fr>
2021-09-10 18:37:39 +0200

    Ajout du fichier header

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudet- <lgaudet-@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/10 18:32:41 by lgaudet-          #+#    #+#             */
+/*   Updated: 2021/09/10 18:36:16 by lgaudet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <termios.h>
+
+#endif
