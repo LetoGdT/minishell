042f88790a89432bdfaaa1447ad559221349ff84
Léto Gaudet dit Trafit <lgaudet-@student.42lyon.fr>
2021-10-06 20:03:10 +0200

    Filtrage de noms de variable d’environnement autorisés

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,22 +6,33 @@
 /*   By: lgaudet- <lgaudet-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 16:09:29 by lgaudet-          #+#    #+#             */
-/*   Updated: 2021/09/23 22:50:51 by lgaudet-         ###   ########.fr       */
+/*   Updated: 2021/10/06 20:02:03 by lgaudet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static int is_valid_name(char *str)
+{
+	if (!str)
+		return (FAILURE);
+	return (ft_isalpha(str[0]) || str[0] == '_');
+}
+
 int	ft_export(int argc, char **argv, t_list **env)
 {
 	int 	i;
-	char	*is_valid;
 
 	i = 0;
 	while (i < argc - 1)
 	{
-		is_valid = ft_strchr(argv[i + 1], '=');
-		if (is_valid && !try_add(argv[i + 1], env))
+		if (!is_valid_name(argv[i + 1]))
+			return (FAILURE);
+			//faut faire de l’affichage d’erreur
+			//bash: export: %s: not a valid identifier
+		if (!ft_strchr(argv[i + 1], '='))
+			continue ;
+		if (!try_add(argv[i + 1], env))
 			return (FAILURE);
 		i++;
 	}
@@ -40,6 +51,10 @@ int	ft_unset(int argc, char **argv, t_list **env)
 			i++;
 			continue ;
 		}
+		if (!is_valid_name(argv[i + 1]))
+			return (FAILURE);
+			//faut faire de l’affichage d’erreur
+			//bash: export: %s: not a valid identifier
 		ft_remove_from_env(argv[i + 1], env);
 		i++;
 	}
